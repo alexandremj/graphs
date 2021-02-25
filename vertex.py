7f6461fb11361f8154439f73a3d32730860f8662
alexandremj <alexandremj@protonmail.com>
2021-02-25 11:59:43 -0300

    Implement __eq__ and __hash__

--- vertex.py
+++ vertex.py
@@ -4,3 +4,10 @@ from dataclasses import dataclass
 class Vertex:
     number: int
     label: str
+
+    def __eq__(self, other):
+        return (other and self.number == other.number
+                and self.label == other.label)
+    
+    def __hash__(self):
+        return hash(self.number)
